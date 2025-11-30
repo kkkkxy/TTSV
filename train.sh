c91d891a31b2f995dba9a1657cdda29eeeeaa2ef
Xinyue Kang <81502045+kkkkxy@users.noreply.github.com>
2025-11-30 18:01:02 +0800

    Update model name and path to Qwen2.5-Math-7B

--- train.sh
+++ train.sh
@@ -7,14 +7,14 @@ accelerate launch \
   --mixed_precision 'bf16' \
   --dynamo_backend 'no' \
   train.py \
-  --model_name Qwen2.5-Math-1.5B \
-  --model_path model/Qwen2.5-Math-1.5B \
+  --model_name Qwen2.5-Math-7B \
+  --model_path model/Qwen2.5-Math-7B \
   --train_dataset_name 'math500' \
   --batch_size 8 \
   --learning_rate 1e-3 \
   --num_epochs 20 \
   --log_steps 1 \
-  --run_name math500_Qwen2.5-Math-1.5B_bs_16_lr_1e-3_wd_1e-8_eps_1e-5_epochs_30_256 \
+  --run_name qwen2.5-math-7b_math500 \
   --save_root 'checkpoints' \
   --prefix_length 20
 
